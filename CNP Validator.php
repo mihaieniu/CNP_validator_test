73220564dd4ed408eacde5c16797a180fb554f47
Mihai E <mihai@htg-software.com>
2022-02-08 01:32:58 +0200

    fixed county list (copied list from instructions PDF)

--- CNP Validator.php
+++ CNP Validator.php
@@ -43,12 +43,14 @@ function isCnpValid(string $value): bool
                     if ($separatedCnp[3] >= "01" && $separatedCnp[3] <= cal_days_in_month(CAL_GREGORIAN, $separatedCnp[2], $cnpFullFormatYear)) { //day variable validity cheking | including check for unequal months and leap years
                         if($separatedCnp[4] >= "01" && $separatedCnp[4] <= "52"){
                             //Create array to identify county
-                            $countyArray = Array("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Braila", "Brasov", "Bucuresti", "Buzau", "Calarasi", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dimbovita", "Dolj", "Galati", "Gorj", "Giurgiu", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Salaj", "Satu Mare", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Vaslui", "Vilcea", "Vrancea");
+                            $countyArray = Array("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Brasov", "Braila", "Buzau", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dimbovita", "Dolj", "Galati", "Gorj", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Satu Mare", "Salaj", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Vaslui", "Valcea", "Vrancea", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Calarasi", "Giurgiu");
                             //index adjustment
-                            $countySelector = $separatedCnp[4+1];
+                            $countySelector = (int)$separatedCnp[4]-1;
+                            if($separatedCnp[5] >= "001" && $separatedCnp[5] <= "999"){ //check for NNN
+                                echo $sexCnp." born on " . $separatedCnp[3] . "." . $separatedCnp[2] . ".19" . $separatedCnp[1] . " in ".$countyArray[$countySelector]." county".PHP_EOL;
+                                $validStatus = true;
+                            }
 
-                            echo $sexCnp." born on " . $separatedCnp[3] . "." . $separatedCnp[2] . ".19" . $separatedCnp[1] . " in ".$countyArray[$countySelector]." county".PHP_EOL;
-                            $validStatus = true;
                         }
                     }
                 }
@@ -60,4 +62,4 @@ function isCnpValid(string $value): bool
 
 }
 
-isCnpValid(2950228125791);
+isCnpValid(2970604203671);
