08f024d54fdd270a49f0b8adf2f047ebdb3e19f2
Mihai E <mihai@htg-software.com>
2022-02-08 00:16:35 +0200

    Added White space check... added numeric value check... added chunk separation

--- CNP Validator.php
+++ CNP Validator.php
@@ -11,9 +11,25 @@ function isCnpValid(string $value): bool {
     //start from asumption that CNP is false
     $validStatus = false;
 
-    print_r($value);
+    //remove all white spaces in case of error
+    $value = trim($value);
 
-    return $validStatus;
+    //check if string only contains numeric values
+    if (ctype_digit($value)) {
+
+        //separate string into chunks by position and length of chunk
+        $separatedCnp[0] = substr($value, 0, 1);
+        $separatedCnp[1] = substr($value, 1, 2 );
+        $separatedCnp[2] = substr($value, 3, 2 );
+        $separatedCnp[3] = substr($value, 5, 2 );
+        $separatedCnp[4] = substr($value, 7, 2 );
+        $separatedCnp[5] = substr($value, 9, 3 );
+        $separatedCnp[6] = substr($value, 12, 1 );
+
+
+        print_r($separatedCnp);
+    }
+        return $validStatus;
 }
 
 isCnpValid(1950112125791);
