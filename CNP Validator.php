b26b450f5eb48aaaddce55cfef96f6c7578eedfe
Mihai E <mihai@htg-software.com>
2022-02-08 00:12:29 +0200

    Added function structure

--- CNP Validator.php
+++ CNP Validator.php
@@ -6,3 +6,14 @@
  * @author Mihai Eniu <mihai@htg-software.com>
  */
 
+function isCnpValid(string $value): bool {
+
+    //start from asumption that CNP is false
+    $validStatus = false;
+
+    print_r($value);
+
+    return $validStatus;
+}
+
+isCnpValid(1950112125791);
