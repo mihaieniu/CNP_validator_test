3c77a90fe150e23139f87d114af824438f02a24b
Mihai E <mihai@htg-software.com>
2022-02-08 18:30:11 +0200

    initialised S, YY, MM, DD value creation

--- CNP_tester.php
+++ CNP_tester.php
@@ -1,8 +1,35 @@
 <?php
 
-
 function generateMultipleCnp (int $numberOfRepeats) : Array {
     $cnpArray = [];
+
+    for($x=1; $x<=$numberOfRepeats; $x++){
+        $sValue = rand(1,9);
+
+        $currentYear = date("y");
+        $currentMonth = date("m");
+        $currentDay = date("d");
+
+        if($sValue == 1 || $sValue == 2){
+            $yValue = rand(0,99);
+            if($yValue<10){
+                $yValue = "0".$yValue;
+            }
+            $yFullValue = "19".$yValue;
+            $mValue = rand(1,12);
+            if($mValue<10){
+                $mValue = "0".$mValue;
+            }
+            $dValue = rand(0,cal_days_in_month(CAL_GREGORIAN, $mValue, $yFullValue));
+            if($dValue < 10){
+                $dValue = "0".$dValue;
+            }
+
+
+        }
+
+    }
+
 
     return $cnpArray;
 }
