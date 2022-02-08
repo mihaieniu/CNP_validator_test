c99dbeff9f9f123cf86e847f3a0779a44f94747f
Mihai E <mihai@htg-software.com>
2022-02-08 03:01:49 +0200

    finished condensing the nested if statements

--- CNP Validator.php
+++ CNP Validator.php
@@ -27,214 +27,27 @@ function isCnpValid(string $value): bool
         $separatedCnp[5] = substr($value, 9, 3);
         $separatedCnp[6] = substr($value, 12, 1);
 
-        $currentYear = date("y");
 
-        //begin 20th century nested if statement
-        if ($separatedCnp[0] == "1" || $separatedCnp[0] == "2" || $separatedCnp[0] == "7" || $separatedCnp[0] == "8" || $separatedCnp[0] == "9") { //sex check for the 20th century
-            //identify sex for troubleshooting
-            if ($separatedCnp[0] % 2 == 1) {
-                $sexCnp = "M";
-            } else {
-                $sexCnp = "F";
-            }
-            if($separatedCnp[0] == 7 || $separatedCnp[0] == 8){
-                $sexCnp = "Foreign citizen ".$sexCnp;
-            }
-            if($separatedCnp[0] == 9){
-                $sexCnp = "Foreign citizen";
-            }
-            if ($separatedCnp[1] >= "00" && $separatedCnp[1] <= "99") { //year checking
-                if ($separatedCnp[2] >= "01" && $separatedCnp[2] <= "12") { //month checking
-                    $cnpFullFormatYear = "19" . $separatedCnp[1];
-                    if ($separatedCnp[3] >= "01" && $separatedCnp[3] <= cal_days_in_month(CAL_GREGORIAN, $separatedCnp[2], $cnpFullFormatYear)) { //day variable validity cheking | including check for unequal months and leap years
-                        if ($separatedCnp[4] >= "01" && $separatedCnp[4] <= "52") {
-                            //Create array to identify county
-                            $countyArray = array("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Brasov", "Braila", "Buzau", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dimbovita", "Dolj", "Galati", "Gorj", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Satu Mare", "Salaj", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Vaslui", "Valcea", "Vrancea", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Calarasi", "Giurgiu");
-                            //index adjustment
-                            $countySelector = (int)$separatedCnp[4] - 1;
-                            if ($separatedCnp[5] >= "001" && $separatedCnp[5] <= "999") { //check for NNN
-                                //logic for Control Number
-                                $controlNumber = 279146358279;
-                                $controlNumberSplit = str_split($controlNumber);
-                                $cnpAsInt = (int)$value;
-                                $cnpAsIntSplit = str_split($cnpAsInt);
-                                $controlSum = 0;
-
-                                for ($x = 0; $x < 12; $x++) {
-                                    $digitMultiplication = $controlNumberSplit[$x] * $cnpAsIntSplit[$x];
-                                    $controlSum += $digitMultiplication;
-                                }
-
-                                $controlSumRemainder = $controlSum % 11;
-
-                                if ($controlSumRemainder == 10 || $controlSumRemainder == 0) {
-                                    $controlSumRemainder = 1;
-                                }
-
-                                if ($separatedCnp[6] == $controlSumRemainder) { //check control number validity
-                                    echo $sexCnp . " born on " . $separatedCnp[3] . "." . $separatedCnp[2] . ".19" . $separatedCnp[1] . " in " . $countyArray[$countySelector] . " county" . PHP_EOL;
-                                    $validStatus = true;
-                                    echo "CNP is valid" . PHP_EOL;
-                                } else {
-                                    echo "Control number is not correct. CNP is NOT valid" . PHP_EOL;
-                                }
-                            } else {
-                                echo "Unique number is out of range. CNP is NOT valid" . PHP_EOL;
-                            }
-                        } else {
-                            echo "County number is out of range. CNP is NOT valid" . PHP_EOL;
-                        }
-                    } else {
-                        echo "Day number is out of range. CNP is NOT valid" . PHP_EOL;
-                    }
-                } else {
-                    echo "Month number is out of range. CNP is NOT valid" . PHP_EOL;
-                }
-            } else {
-                echo "Year number is out of range. CNP is NOT valid" . PHP_EOL;
-            }
-        }
-        //begin 19th century nested if statement
-        elseif ($separatedCnp[0] == "3" || $separatedCnp[0] == "4") { //sex check for the 20th century
-            //identify sex for troubleshooting
-            if ($separatedCnp[0] % 2 == 1) {
-                $sexCnp = "M";
-            } else {
-                $sexCnp = "F";
-            }
-            if ($separatedCnp[1] >= "00" && $separatedCnp[1] <= "99") { //year checking
-                if ($separatedCnp[2] >= "01" && $separatedCnp[2] <= "12") { //month checking
-                    $cnpFullFormatYear = "18" . $separatedCnp[1];
-                    if ($separatedCnp[3] >= "01" && $separatedCnp[3] <= cal_days_in_month(CAL_GREGORIAN, $separatedCnp[2], $cnpFullFormatYear)) { //day variable validity cheking | including check for unequal months and leap years
-                        if ($separatedCnp[4] >= "01" && $separatedCnp[4] <= "52") {
-                            //Create array to identify county
-                            $countyArray = array("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Brasov", "Braila", "Buzau", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dimbovita", "Dolj", "Galati", "Gorj", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Satu Mare", "Salaj", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Vaslui", "Valcea", "Vrancea", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Calarasi", "Giurgiu");
-                            //index adjustment
-                            $countySelector = (int)$separatedCnp[4] - 1;
-                            if ($separatedCnp[5] >= "001" && $separatedCnp[5] <= "999") { //check for NNN
-                                //logic for Control Number
-                                $controlNumber = 279146358279;
-                                $controlNumberSplit = str_split($controlNumber);
-                                $cnpAsInt = (int)$value;
-                                $cnpAsIntSplit = str_split($cnpAsInt);
-                                $controlSum = 0;
-
-                                for ($x = 0; $x < 12; $x++) {
-                                    $digitMultiplication = $controlNumberSplit[$x] * $cnpAsIntSplit[$x];
-                                    $controlSum += $digitMultiplication;
-                                }
-
-                                $controlSumRemainder = $controlSum % 11;
-
-                                if ($controlSumRemainder == 10 || $controlSumRemainder == 0) {
-                                    $controlSumRemainder = 1;
-                                }
-
-                                if ($separatedCnp[6] == $controlSumRemainder) { //check control number validity
-                                    echo $sexCnp . " born on " . $separatedCnp[3] . "." . $separatedCnp[2] . ".18" . $separatedCnp[1] . " in " . $countyArray[$countySelector] . " county" . PHP_EOL;
-                                    $validStatus = true;
-                                    echo "CNP is valid" . PHP_EOL;
-                                } else {
-                                    echo "Control number is not correct. CNP is NOT valid" . PHP_EOL;
-                                }
-                            } else {
-                                echo "Unique number is out of range. CNP is NOT valid" . PHP_EOL;
-                            }
-                        } else {
-                            echo "County number is out of range. CNP is NOT valid" . PHP_EOL;
-                        }
-                    } else {
-                        echo "Day number is out of range. CNP is NOT valid" . PHP_EOL;
-                    }
-                } else {
-                    echo "Month number is out of range. CNP is NOT valid" . PHP_EOL;
-                }
-            } else {
-                echo "Year number is out of range. CNP is NOT valid" . PHP_EOL;
-            }
-        }
-        //begin 21st century nested if statement
-        elseif ($separatedCnp[0] == "5" || $separatedCnp[0] == "6") { //sex check for the 20th century
-            //identify sex for troubleshooting
-            if ($separatedCnp[0] % 2 == 1) {
-                $sexCnp = "M";
-            } else {
-                $sexCnp = "F";
-            }
-            if ($separatedCnp[1] >= "00" && $separatedCnp[1] <= $currentYear) { //year checking up to current year
-                if ($separatedCnp[2] >= "01" && $separatedCnp[2] <= "12") { //month checking
-                    $cnpFullFormatYear = "20" . $separatedCnp[1];
-                    if ($separatedCnp[3] >= "01" && $separatedCnp[3] <= cal_days_in_month(CAL_GREGORIAN, $separatedCnp[2], $cnpFullFormatYear)) { //day variable validity cheking | including check for unequal months and leap years
-                        if ($separatedCnp[4] >= "01" && $separatedCnp[4] <= "52") {
-                            //Create array to identify county
-                            $countyArray = array("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Brasov", "Braila", "Buzau", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dimbovita", "Dolj", "Galati", "Gorj", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Satu Mare", "Salaj", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Vaslui", "Valcea", "Vrancea", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Bucuresti", "Calarasi", "Giurgiu");
-                            //index adjustment
-                            $countySelector = (int)$separatedCnp[4] - 1;
-                            if ($separatedCnp[5] >= "001" && $separatedCnp[5] <= "999") { //check for NNN
-                                //logic for Control Number
-                                $controlNumber = 279146358279;
-                                $controlNumberSplit = str_split($controlNumber);
-                                $cnpAsInt = (int)$value;
-                                $cnpAsIntSplit = str_split($cnpAsInt);
-                                $controlSum = 0;
-
-                                for ($x = 0; $x < 12; $x++) {
-                                    $digitMultiplication = $controlNumberSplit[$x] * $cnpAsIntSplit[$x];
-                                    $controlSum += $digitMultiplication;
-                                }
-
-                                $controlSumRemainder = $controlSum % 11;
-
-                                if ($controlSumRemainder == 10 || $controlSumRemainder == 0) {
-                                    $controlSumRemainder = 1;
-                                }
-
-                                if ($separatedCnp[6] == $controlSumRemainder) { //check control number validity
-                                    echo $sexCnp . " born on " . $separatedCnp[3] . "." . $separatedCnp[2] . ".20" . $separatedCnp[1] . " in " . $countyArray[$countySelector] . " county" . PHP_EOL;
-                                    $validStatus = true;
-                                    echo "CNP is valid" . PHP_EOL;
-                                } else {
-                                    echo "Control number is not correct. CNP is NOT valid" . PHP_EOL;
-                                }
-                            } else {
-                                echo "Unique number is out of range. CNP is NOT valid" . PHP_EOL;
-                            }
-                        } else {
-                            echo "County number is out of range. CNP is NOT valid" . PHP_EOL;
-                        }
-                    } else {
-                        echo "Day number is out of range. CNP is NOT valid" . PHP_EOL;
-                    }
-                } else {
-                    echo "Month number is out of range. CNP is NOT valid" . PHP_EOL;
-                }
-            } else {
-                echo "Year number is out of range. CNP is NOT valid" . PHP_EOL;
-            }
-        }
-        //final control message
-        else{
-            echo "Sex integer is out of range".PHP_EOL;
-        }
-
-        //begin 20th century nested if statement
+        //begin condensed nested if statement
         if ($separatedCnp[0] >= "1" && $separatedCnp[0] <= 9) { //sex check
-            //identify sex for troubleshooting
 
+            $currentYear = date("y");
             $cnpFullFormatYear = "19" . $separatedCnp[1];
 
+            //identify sex for troubleshooting
             if ($separatedCnp[0] % 2 == 1) {
                 $sexCnp = "M";
             } else {
                 $sexCnp = "F";
             }
+            //add foreign citizen tag
             if($separatedCnp[0] == 7 || $separatedCnp[0] == 8){
                 $sexCnp = "Foreign citizen ".$sexCnp;
             }elseif($separatedCnp[0] == 9){
                 $sexCnp = "Foreign citizen";
             }
 
-
+            //adjustment for different centuries
             if($separatedCnp[0] == 5 || $separatedCnp[0] == 6){
                 $cnpFullFormatYear = "20" . $separatedCnp[1];
                 if($separatedCnp[1] > $currentYear){
@@ -295,11 +108,8 @@ function isCnpValid(string $value): bool
                 echo "Year number is out of range. CNP is NOT valid" . PHP_EOL;
             }
         }
-
-
     }
         return $validStatus;
-
 }
 //check for 20th Century
 isCnpValid(1950112125791);
